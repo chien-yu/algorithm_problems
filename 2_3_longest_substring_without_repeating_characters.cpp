7a9398f738ab156511649bdc1ca39e2ea4389484
Chien-Yu Chan <chan.chienyuc@gmail.com>
2023-07-25 01:39:05 -0700

    2_3 again, need review

--- 2_3_longest_substring_without_repeating_characters.cpp
+++ 2_3_longest_substring_without_repeating_characters.cpp
@@ -68,3 +68,73 @@ public:
         return ans;
     }
 };
+
+// 7/25/2023
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // sliding window
+        // prepare a map to count each character
+        // input space is ASCII?
+        // if there are any char count larger than 1, we start to move left pointer
+
+        if (s.size() <= 1)
+            return s.size();
+        if (s.size() == 2)
+            return s[0] == s[1] ? 1 : 2;
+        int i = 0, j = 0;
+        unordered_map<char, int> buf;
+
+        int answer = 0;
+        while (i <= j && j < s.size())
+        {
+            // start from left, move j until counter larger 1
+            // or until j == size - 1 (right-most)
+            char dupC = '\0';
+            for (; j < s.size(); j++)
+            {
+                if (buf.count(s[j]) == 0 || buf[s[j]] == 0)
+                {
+                    buf[s[j]] = 1;
+                    answer = answer < (j - i + 1) ? (j - i + 1) : answer;
+                }
+                else
+                {
+                    buf[s[j]]++;
+                    dupC = s[j];
+                    // cout << dupC << endl;
+                    break;
+                }
+            }
+
+            // move i
+            if (dupC != '\0')
+            {
+                for (; i <= j; i++)
+                {
+                    buf[s[i]]--;
+                    if (s[i] == dupC)
+                    {
+                        if (buf[s[i] < 0])
+                        {
+                            assert(false);
+                        }
+                        i++;
+                        break;
+                    }
+                }
+            }
+
+            // handle okay, back to move j
+            j++;
+        }
+        return answer;
+    }
+};
